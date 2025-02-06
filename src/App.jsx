1f015c90a2b2364d357083f3573df388cb5717b2
Pradhanji09 <sou030rav@gmail.com>
2025-02-06 13:31:27 +0530

    Basic structure completed

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import Chaboticon from "./components/Chaboticon";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        {/* Chatbot Header*/}
+        <div className="chatbot-header">
+          <div className="header-info">
+            <Chaboticon />
+            <h2 className="logo-text ">Chatbot </h2>
+          </div>
+          <button className="material-symbols-outlined">
+            keyboard_arrow_down
+          </button>
+        </div>
+
+        {/* Chatbot Body*/}
+        <div className="chatbot-body">
+          <div className="message bot-message">
+            <Chaboticon />
+            <p className="message-text">
+              Hey there <br /> How can I help you today?
+            </p>
+          </div>
+          <div className="message user-message">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+
+        {/* Chatbot Footer*/}
+        <div className="chatbot-footer">
+          <form action="#" className="chat-form">
+            <input
+              type="text"
+              placeholder="Message..."
+              className="message-input"
+              required
+            />
+            <button className="material-symbols-outlined">arrow_upward</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
